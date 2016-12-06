da8e37bff1a73c39e31eeb2733e9f6b8093c55da
Destiny262 <andrew_johnson262@hotmail.com>
2016-12-06 09:34:55 -0500

    Model Library to use as an interface
    Temporary fix to not being able to use the database.

---
+++ ModelLibrary/SalesDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    public class SalesDiscount
+    {
+        public SalesDiscount()
+        {
+
+        }
+
+        public int salesDiscountId { get; set; }
+        public double salesDiscount { get; set; } 
+
+    }
+}
